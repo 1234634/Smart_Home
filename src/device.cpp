fdbfd461cab9847547313f009895a0a580273f3a
1234634 <49291996+1234634@users.noreply.github.com>
2020-05-29 22:49:06 +0200

    Add files via upload

--- src/device.cpp
+++ src/device.cpp
@@ -1,1 +1,180 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <map>
+#include <iterator>
+#include<bits/stdc++.h>
+
+#define DATA_PORT 8080
+
+using namespace std;
+
+class Device
+{
+    private:
+        string room; // room in which is device
+        string device_name;
+        map<string, string> device_data;// name of data and data itself i.e. map<"TEMPERATURE", "30"> normaly device will have only one
+        string device_data_message; // message explaining basic informations about device , name, in which room is it, written in requiered ssdp format
+        string data_port;
+        mutex m; // mutex used for mutalexclusive acces to device data
+    public:
+
+        Device()
+        {
+            this->device_name = "device";
+            this->room = "LIVING ROOM";
+            this->device_data["TEMPERATURE"] = "0";
+            this->data_port = to_string(DATA_PORT);
+        }
+
+        Device(string d_name,string r,string dd1,string dd2,string dp)
+        {
+            this->device_name=d_name;
+            this->room=r;
+            this->device_data[dd1]=dd2;
+            this->data_port=dp;
+        }
+
+	 void set_device_name(string device_name)
+        {
+            this->device_name = device_name;
+        }
+
+        void set_room(string room)
+        {
+            this->room = room;
+        }
+
+        void set_port(string port)
+        {
+            this->data_port = port;
+        }
+
+
+        string get_room()
+        {
+            return room;
+        }
+
+	string get_device_name()
+        {
+            return this->device_name;
+        }
+
+
+        string get_topic_value(string topic)
+        {
+            return this->device_data[topic];
+        }
+	
+	map<string,string> get_device_data()
+        {
+            return this->device_data;
+        }
+	
+	string get_data_port()
+        {
+            return this->data_port;
+        }
+
+
+        string get_all_topics()
+        {
+	    // device topici sta on ima u sebi ("temperature"...)
+            string topics;
+            for(auto it = this->device_data.begin(); it != this->device_data.end(); it++)
+            {
+                topics += it->first;
+                topics += ",";
+            }
+
+            topics[topics.length() - 1] = '\0';
+            return topics;
+        }
+
+
+
+        void set_topic_value(string key, string value)
+        {
+            m.lock();
+            this->device_data[key] = value;
+            m.unlock();
+        }
+
+
+        bool topic_exist(string topic)
+        {
+            map<string, string>::iterator it = this->device_data.find(topic);
+            if(it != this->device_data.end())
+                return true;
+
+            return false;
+        }
+};
+
+Device* make_device(string message)
+{
+
+        string search_message = message;
+
+        string delimiter = "|";
+        cout << search_message << endl;
+
+        string device_name = search_message.substr(0, search_message.find(delimiter));
+        search_message.erase(0, search_message.find(delimiter) + delimiter.length());
+
+        string room = search_message.substr(0, search_message.find(delimiter));
+        search_message.erase(0, search_message.find(delimiter) + delimiter.length());
+
+        string device_data1 = search_message.substr(0, search_message.find(delimiter));
+        search_message.erase(0, search_message.find(delimiter) + delimiter.length());
+
+        string device_data2 = search_message.substr(0, search_message.find(delimiter));
+        search_message.erase(0, search_message.find(delimiter) + delimiter.length());
+
+        string data_port = search_message.substr(0, search_message.find(delimiter));
+        search_message.erase(0, search_message.find(delimiter) + delimiter.length());
+
+        Device *prepared_device = new Device(device_name,room,device_data1,device_data2,data_port);
+
+
+        return prepared_device;
+
+}
+      
+
+string make_device_info_message(Device* device)
+{
+
+    string return_value = device->get_device_name() + "|" + device->get_room() + "|";
+
+    for(auto it = device->get_device_data().begin(); it != device->get_device_data().end(); it++)
+    {
+        return_value += it->first;
+        return_value += "|";
+	return_value += it->second;
+	return_value += '|';   
+    }
+
+    
+    return_value += device->get_data_port();
+    return return_value;
+}
+
+int main()
+{
+    string mes = "A|B|C|D|E";
+    Device *temp = make_device(mes);
+    string a = temp->get_device_name();
+    cout << make_device_info_message(temp);
+
+    return 0;
+}
+
 
---
+++ src/kontroler.cpp
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <thread>
+#include <iterator>
+#include<bits/stdc++.h>
+#include <mutex>
+#include <map>
+#include"device.cpp"
+using namespace std;
+
+//ports
+#define BROADCAST_PORT 8080
+#define LISTEN_PORT BROADCAST_PORT + 1
+#define DATA_PORT LISTEN_PORT + 1
+
+
+//len
+#define FOUND_MESSAGE_LEN 20
+#define DEFAULT_BUFLEN 512
+
+//bool found = false;
+
+//const char found_message[] = "Device found!";
+
+//list of sensors
+const string fingerprint_sensor = "SWITCH";
+const string oven_sensor = "OVEN_SENSOR";
+const string temperature_sensor = "TEMPERATURE_SENSOR";
+const string air_conditioning = "AIR_CONDITIONING";
+const string door_opener = "DOOR_OPENER";
+const string light_bulb = "LIGHT_BULB";
+
+
+
+
+
+
+
+map<string,Device*> device_map;
+
+
+void find_devices()
+{
+
+    struct sockaddr_in address;
+    int read_size, broadcast_socket;
+    char message[DEFAULT_BUFLEN];
+    string search_message;
+    socklen_t address_size = sizeof(address);
+
+    broadcast_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(broadcast_socket == -1)
+    {
+        perror("Broadcast socket creation failed");
+        return;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(BROADCAST_PORT);
+
+    if(bind(broadcast_socket, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("Failed to bind udp socket.\n");
+        return;
+    }
+
+    while((read_size = recvfrom(broadcast_socket , message , DEFAULT_BUFLEN , 0, (struct sockaddr*)&address, &address_size)) > 0 )
+    {
+        message[read_size] = '\0';
+        search_message = message;
+        thread add_device_thread(add_device, ref(search_message));
+        add_device_thread.detach();
+    }
+}
+
+
+void get_device_parameters(Device device)
+{
+    //uzeti port iz sensor
+    //napraviti update sensor funkciju koja update sve topice u sensor.map
+    //pozvati fju
+
+    //while 1 recv parsirati poruku
+    //pozvati update sensor funkciju u threadu!!!!! thread d(parametri), d.detach()!!!!!!!!!!
+}
+
+void update_device(Device device)
+{
+    //postavlja sve vrednosti topica u mapi na date vrednosti;
+}
+
+
+
+/*int main()
+{
+
+
+    find_devices();
+// thread find sensors
+
+
+
+
+//join find sensors thread
+
+
+}*/
